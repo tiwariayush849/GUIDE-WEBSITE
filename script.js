322b750a231177cfffdd7373de7357818b66a306
tiwariayush849 <139482203+tiwariayush849@users.noreply.github.com>
2024-06-21 00:36:21 +0530

    Add files via upload
    Welcome to GUIDE, the premier destination for all your travel needs. Whether you're an adventurous solo traveler, a couple seeking a romantic getaway, or a family planning an unforgettable vacation, GUIDE is here to make your travel dreams a reality.

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+const navToggleEvent = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+};
+
+navElemArr.forEach(elem => {
+  elem.addEventListener("click", navToggleEvent);
+});
+
+/**
+ * header and go top button active when scroll down to 200px
+ */
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 200) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+});
